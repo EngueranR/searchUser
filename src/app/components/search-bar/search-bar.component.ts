fde429ac459f29fef75e34aa107aff8e72694c0d
engueranraout <rt.engueran@gmail.com>
2024-07-22 11:24:05 +0200

    chore: Update dependencies and add fontawesome CSS feat: dynamisation search

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css'],
 })
-export class SearchBarComponent {}
+export class SearchBarComponent {
+  @Output() search = new EventEmitter<string>();
+
+  searchTerm: string = '';
+
+  onSearch() {
+    this.search.emit(this.searchTerm);
+  }
+}
