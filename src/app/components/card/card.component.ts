60b2c11dedf608591fac72a5f58e71ecf2136f12
engueranraout <rt.engueran@gmail.com>
2024-07-22 09:35:22 +0200

    feat: Add user services and components for user page and user ID

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { UserIdService } from 'src/app/services/users/user-id.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
+  constructor(private userIdService: UserIdService) {}
 
+  ngOnInit() {
+    this.userIdService.getUserById(56190).then((data) => {
+      console.log(data);
+    });
+  }
 }
--- src/app/components/grid-card/grid-card.component.ts
+++ src/app/components/grid-card/grid-card.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { UserIdService } from 'src/app/services/users/user-page.service';
 @Component({
   selector: 'app-grid-card',
   templateUrl: './grid-card.component.html',
-  styleUrls: ['./grid-card.component.css']
+  styleUrls: ['./grid-card.component.css'],
 })
 export class GridCardComponent {
-
+  constructor(private userPageService: UserIdService) {
+    this.userPageService.postPageByNumber(1).then((data) => {
+      console.log(data);
+    });
+  }
 }
