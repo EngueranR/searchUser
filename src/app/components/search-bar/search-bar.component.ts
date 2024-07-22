d24ba7c7ead069fb3919f4249c9862144f28c0c5
engueranraout <rt.engueran@gmail.com>
2024-07-22 12:28:38 +0200

    chore: Update search-bar component UI and functionality

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -6,11 +6,20 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
-  @Output() search = new EventEmitter<string>();
-
+  @Output() search = new EventEmitter<string[]>();
   searchTerm: string = '';
+  terms: string[] = [];
 
   onSearch() {
-    this.search.emit(this.searchTerm);
+    if (this.searchTerm.trim()) {
+      this.terms.push(this.searchTerm.trim());
+      this.searchTerm = '';
+      this.search.emit(this.terms);
+    }
+  }
+
+  removeTerm(index: number) {
+    this.terms.splice(index, 1);
+    this.search.emit(this.terms);
   }
 }
